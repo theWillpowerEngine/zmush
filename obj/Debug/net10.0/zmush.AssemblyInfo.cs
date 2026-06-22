2796f983f2394d1e7aeb86d74093289459c40274
malf <donteatthelotus@proton.me>
2026-06-21 20:06:06 -0700

    I can't believe in this year of our lord 2026 that VSCode's "publish to github" feature includes binaries holy mother of god.

--- obj/Debug/net10.0/zmush.AssemblyInfo.cs
+++ obj/Debug/net10.0/zmush.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("zmush")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda936d7232bbf28d94462625a9c61bacfc84e9f")]
 [assembly: System.Reflection.AssemblyProductAttribute("zmush")]
 [assembly: System.Reflection.AssemblyTitleAttribute("zmush")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
